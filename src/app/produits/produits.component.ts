b09088b539e12e5d584b05a82faac09fbe2c27b8
maher mansour <105998561+mahermansour47455@users.noreply.github.com>
2022-10-30 18:51:36 +0100

    Add files via upload

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
+})
+export class ProduitsComponent implements OnInit {
+  produits? : Produit[]; //un tableau de Produit
+
+  constructor(private produitService: ProduitService ,
+    private router:Router) {
+   // this.produits = this.produitService.listeProduits();
+    
+ 
+    }
+  ngOnInit(): void {
+
+    this.produitService.listeProduit().subscribe(prods => {
+      console.log(prods);
+      this.produits = prods;
+      });
+  }
+  supprimerProduit(p: Produit)
+{
+let conf = confirm("Etes-vous sûr ?");
+if (conf)
+this.produitService.supprimerProduit(p.idProduit!).subscribe(() => {
+console.log("produit supprimé");
+});
+this.router.navigate(['produits']).then(() => {
+window.location.reload();
+this.SuprimerProduitDuTableau(p);
+});
+}
+SuprimerProduitDuTableau(prod : Produit) {
+  this.produits!.forEach((cur, index) => {
+  if(prod.idProduit=== cur.idProduit) {
+  this.produits!.splice(index, 1);
+  }
+  });
+  }
+
+}
